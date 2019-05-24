1b6d6afff425ecdfff514bb1e2e3f56f79184cec
tttzach <tttzach@gmail.com>
2019-05-24 00:15:18 -0400

    Complete implementation; Unix executable created

--- main.c
+++ main.c
@@ -30,33 +30,33 @@ bool full(int board[9]) {
 // state in favor of the player specified
 bool win(int board[9], int player) {
   if ((board[0] == player &&
-       board[3] == player &&
-       board[6] == player) ||
-      (board[1] == player &&
-       board[4] == player &&
-       board[7] == player) ||
-      (board[2] == player &&
-       board[5] == player &&
-       board[8] == player) ||
-      (board[0] == player &&
-       board[1] == player &&
-       board[2] == player) ||
-      (board[3] == player &&
-       board[4] == player &&
-       board[5] == player) ||
-      (board[6] == player &&
-       board[7] == player &&
-       board[8] == player) ||
-      (board[0] == player &&
-       board[4] == player &&
-       board[8] == player) ||
-      (board[2] == player &&
-       board[4] == player &&
+   board[3] == player &&
+   board[6] == player) ||
+    (board[1] == player &&
+     board[4] == player &&
+     board[7] == player) ||
+    (board[2] == player &&
+     board[5] == player &&
+     board[8] == player) ||
+    (board[0] == player &&
+     board[1] == player &&
+     board[2] == player) ||
+    (board[3] == player &&
+     board[4] == player &&
+     board[5] == player) ||
+    (board[6] == player &&
+     board[7] == player &&
+     board[8] == player) ||
+    (board[0] == player &&
+     board[4] == player &&
+     board[8] == player) ||
+    (board[2] == player &&
+     board[4] == player &&
        board[6] == player)) { // 8 possible win combinations
     return true;
-  } else {
-    return false;
-  }
+} else {
+  return false;
+}
 }
 
 // minimax(board, scoreboard, player) recursively updates
@@ -140,14 +140,20 @@ void move(int board[9]) {
 }
 
 void print(int board[9]) {
+  printf("\n\n\n\n\n\n                                  TIC TAC TOE\n\n\n");
   int column = 0;
   for (int i = 0; i < size; i++) {
+    if (column == 0) {
+      printf("                                     ");
+    }
     if (board[i] == human) {
-      printf("\u25EF");
+      //printf("\u25EF");
+      printf("\x001b[1mO\x1B[0m");
     } else if (board[i] == robot) {
-      printf("\u2573");
+      //printf("\u2573");
+      printf("\x001b[1mX\x1B[0m");
     } else {
-      printf("%d", i + 1);
+      printf("\x001b[31m%d\x1B[0m", i + 1);
     }
     if (column == 2) {
       printf("\n");
@@ -157,6 +163,7 @@ void print(int board[9]) {
       column++;
     }
   }
+  printf("\n\n");
 }
 
 void update(int board[9]) {
@@ -165,31 +172,83 @@ void update(int board[9]) {
   int ind = c - '0';
   if (board[ind - 1] == empty) {
     board[ind - 1] = human;
+  }
+}
+
+bool restart(void) {
+  char c = getchar();
+  getchar();
+  if (c == 'Y' || c == 'y') {
+    return true;
   } else {
-    printf("Invalid move! You lose your turn!\n");
+    return false;
   }
 }
 
 void game(int board[9]) {
   while (1) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+      if (board[i] != 0) {
+        count++;
+      }
+    }
+    if (count < 8) {
+      system("clear");
+      print(board);
+      printf("                         Type a number and press enter.\n                         If you try to overwrite a box,\n                         your chance will be passed on.\n\n\n");
+      printf("                                       ");
+      update(board);
+      move(board);
+    }
     system("clear");
     print(board);
-    printf("Type a number and press enter.\n");
-    update(board);
-    move(board);
+    if (count == 8) {
+      for (int i = 0; i < size; i++) {
+        if (board[i] == 0) {
+          board[i] = human;
+          continue;
+        }
+      }
+    }
     system("clear");
     print(board);
     if (win(board, human)) {
-      printf("You win!\n");
-      break;
+      printf("                                   You win!\n\n");
+      printf("                         Would you like to replay? Y/N\n\n");
+      if (restart()) {
+        for (int i = 0; i < size; i++) {
+          board[i] = 0;
+        }
+        continue;
+      } else {
+        system("clear");
+        break;
+      }
     } else if (win(board, robot)) {
-      printf("You lose!\n");
-      break;
+      printf("                                    You lose!\n\n");
+      printf("                          Would you like to replay? Y/N\n\n");
+      if (restart()) {
+        for (int i = 0; i < size; i++) {
+          board[i] = 0;
+        }
+        continue;
+      } else {
+        system("clear");
+        break;
+      }
     } else if (full(board)) {
-      printf("Tie!\n");
-      break;
-    } else {
-      printf("Keep going!\n");
+      printf("                                      Tie!\n\n");
+      printf("                         Would you like to replay? Y/N\n\n");
+      if (restart()) {
+        for (int i = 0; i < size; i++) {
+          board[i] = 0;
+        }
+        continue;
+      } else {
+        system("clear");
+        break;
+      }
     }
   }
 }
@@ -197,4 +256,5 @@ void game(int board[9]) {
 int main(void) {
   int ogboard[9] = {0,0,0,0,0,0,0,0,0};
   game(ogboard);
+  return 0;
 }
